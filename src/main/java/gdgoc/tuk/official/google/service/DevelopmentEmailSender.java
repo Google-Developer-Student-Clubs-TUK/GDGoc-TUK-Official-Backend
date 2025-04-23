c81651182c9e8a5f20cfb8d6c688ddb61ca3df80
songhaechan <bukak2019@naver.com>
2025-04-23 16:08:25 +0900

    fix: @Profile name

--- src/main/java/gdgoc/tuk/official/google/service/DevelopmentEmailSender.java
+++ src/main/java/gdgoc/tuk/official/google/service/DevelopmentEmailSender.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("local")
+@Profile("dev")
 @Slf4j
-public class LocalEmailSender implements EmailSender {
+public class DevelopmentEmailSender implements EmailSender {
 
     @Override
     public void send(final String to, final String subject, final String content) {
