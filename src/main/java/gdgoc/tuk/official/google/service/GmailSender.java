621e389339fbf758ef0e90691dcd2dbef3a9929b
songhaechan <bukak2019@naver.com>
2025-06-04 14:32:49 +0900

    feat: custom thread poll

--- src/main/java/gdgoc/tuk/official/google/service/GmailSender.java
+++ src/main/java/gdgoc/tuk/official/google/service/GmailSender.java
@@ -23,26 +23,26 @@ public class GmailSender implements EmailSender {
     @Async
     @Override
     public void send(String to, String subject, String content) {
-//        MimeMessage message = javaMailSender.createMimeMessage();
-//        try {
-//            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
-//            helper.setFrom("GDGoCTUK");
-//            helper.setTo(to);
-//            helper.setSubject(subject);
-//            helper.setText(content, true);
-//            javaMailSender.send(message);
-//        } catch (MessagingException e) {
-//            log.error("MessagingException : {}", e);
-//        } catch (RuntimeException e) {
-//            log.error("Runtime Exception : {}", e);
-//        }
+        MimeMessage message = javaMailSender.createMimeMessage();
         try {
-            log.debug("[MOCK EMAIL START] to={}", to);
-            Thread.sleep(3300);
-            log.debug("[MOCK EMAIL DONE] to={}", to);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            log.error("Mock email sending interrupted", e);
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+            helper.setFrom("GDGoCTUK");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            log.error("MessagingException : {}", e);
+        } catch (RuntimeException e) {
+            log.error("Runtime Exception : {}", e);
         }
+//        try {
+//            log.debug("[MOCK EMAIL START] to={}", to);
+//            Thread.sleep(3300);
+//            log.debug("[MOCK EMAIL DONE] to={}", to);
+//        } catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//            log.error("Mock email sending interrupted", e);
+//        }
     }
 }
