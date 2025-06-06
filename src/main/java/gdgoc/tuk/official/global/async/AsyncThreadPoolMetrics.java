6350d9c3e7e51bae2b483579cba8ab9ee3147f57
songhaechan <bukak2019@naver.com>
2025-06-06 14:59:52 +0900

    feat: Add Custom Metrics

--- src/main/java/gdgoc/tuk/official/global/async/AsyncThreadPoolMetrics.java
+++ src/main/java/gdgoc/tuk/official/global/async/AsyncThreadPoolMetrics.java
@@ -2,12 +2,14 @@ package gdgoc.tuk.official.global.async;
 
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+
 import jakarta.annotation.PostConstruct;
-import java.util.concurrent.ThreadPoolExecutor;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Component
 public class AsyncThreadPoolMetrics {
@@ -15,8 +17,9 @@ public class AsyncThreadPoolMetrics {
     private final ThreadPoolTaskExecutor executor;
     private final MeterRegistry registry;
 
-    public AsyncThreadPoolMetrics(@Qualifier("asyncThreadPoolTaskExecutor") final ThreadPoolTaskExecutor executor,
-        final MeterRegistry registry) {
+    public AsyncThreadPoolMetrics(
+            @Qualifier("asyncThreadPoolTaskExecutor") final ThreadPoolTaskExecutor executor,
+            final MeterRegistry registry) {
         this.executor = executor;
         this.registry = registry;
     }
@@ -26,13 +29,19 @@ public class AsyncThreadPoolMetrics {
         ThreadPoolExecutor tp = executor.getThreadPoolExecutor();
 
         Gauge.builder("async.executor.active.thread", tp, ThreadPoolExecutor::getActiveCount)
-            .description("Active async threads")
-            .register(registry);
+                .description("Active async threads")
+                .register(registry);
 
-//        Gauge.builder("async.executor.live.thread",tp,ThreadPoolExecutor::get)
+        Gauge.builder("async.executor.live.thread", tp, ThreadPoolExecutor::getPoolSize)
+                .description("Thread Pool Size")
+                .register(registry);
+
+        Gauge.builder("async.executor.task.complete", tp, ThreadPoolExecutor::getCompletedTaskCount)
+                .description("Complete Task")
+                .register(registry);
 
         Gauge.builder("async.executor.queue.size", tp, e -> e.getQueue().size())
-            .description("Async executor queue size")
-            .register(registry);
+                .description("Async executor queue size")
+                .register(registry);
     }
 }
